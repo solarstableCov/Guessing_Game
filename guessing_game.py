82915b01e406b0578a2ee45c72177a983589d320
Resume/Coventry University Cybersecurity Bootcamp <you@example.com>
2022-11-11 18:17:39 -0800

    added files

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 import random
-minimum= int(input("Enter Lower bound: "))
-maximum= int(input("Enter Upper bound: "))
+difficulty=input("Choose a difficulty (Easy, Medium, Hard, Hardcore): ")
+if difficulty=="Easy":
+   minimum=0
+   maximum=10
+elif difficulty=="Medium":
+   minimum=0
+   maximum=50
+elif difficulty=="Hard":
+   minimum=0
+   maximum=100
+elif difficulty=="Hardcore":
+   minimum=0
+   maximum=1000
+else:
+   print("enter difficulty again: ")
+minimum= int(minimum)
+maximum= int(maximum)
 answer=random.randint(minimum,maximum)
 print(answer)
 guess=input("enter a number: " )
